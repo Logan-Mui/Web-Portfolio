995801acdc67c69e6c66df9270a9abf26893de0c
Logan Mui <146005376+Logan-Mui@users.noreply.github.com>
2025-10-02 09:12:38 +0200

    shrinking the images manually i'll clutch later

---
+++ src/pages/splash.tsx
@@ -0,0 +1,14 @@
+import Header from "../components/header";
+import Menu from "../components/menu";
+
+function Splash() {
+
+  return (
+    <>
+      <Menu></Menu>
+      <Header></Header>
+    </>
+  )
+}
+
+export default Splash;
