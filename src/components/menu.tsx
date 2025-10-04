7092483a147dcd56199c3c1047825f940d697603
Logan Mui <146005376+Logan-Mui@users.noreply.github.com>
2025-10-04 18:16:55 +0200

    going to see if this splash menu styling will work

--- src/components/menu.tsx
+++ src/components/menu.tsx
@@ -1,12 +1,20 @@
+import pork from "../assets/photos/pork.jpg";
+import "../styles/splash/splash.css";
+
 function Menu() {
 
   return (
-    <div className="Menu">
-        <a href="">COMMUNITY</a>
-        <a href="">ABOUT ME</a>
-        <a href="">WORK/CV</a>
-        <a href="">PROJECTS</a>
-    </div>
+    <>
+      <div className="SplashMenu">
+          <a href="">COMMUNITY</a>
+          <a href="">ABOUT ME</a>
+          <a href="">WORK/CV</a>
+          <a href="">PROJECTS</a>
+      </div>
+      <div className="PicOfMe">
+          <img src={pork}></img>
+      </div>
+    </>
   )
 }
 
