868b019a9a168958bcc7ad6c9861f8c43c7a2e08
Logan Mui <l.mui@blindleister.de>
2025-10-16 20:18:15 +0200

    adding sliding show of photos

--- src/components/menu.tsx
+++ src/components/menu.tsx
@@ -1,18 +1,41 @@
-import pork from "../assets/photos/pork.jpg";
+import { useState, useEffect, type JSX } from 'react';
+import headshot from "../assets/photos/headshot.jpg";
+import singing from "../assets/photos/singing.jpg";
+import career_fair from "../assets/photos/career_fair.jpg";
+import bear from "../assets/photos/bear.jpg";
 import "../styles/splash/menu.css";
 
-function Menu() {
+function Menu(): JSX.Element {
+  const images: string[] = [headshot, singing career_fair, bear];
+  const [currentImageIndex, setCurrentImageIndex] = useState<number>(0);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setCurrentImageIndex((prevIndex: number) => 
+        (prevIndex + 1) % images.length
+      );
+    }, 3000); // Change image every 3 seconds
+
+    return () => clearInterval(interval);
+  }, [images.length]);
 
   return (
     <div className="Menu">
       <div className="MenuLinks">
-          <a href="">COMMUNITY</a>
-          <a href="">ABOUT ME</a>
-          <a href="">WORK/CV</a>
-          <a href="">PROJECTS</a>
+        <a href="">COMMUNITY</a>
+        <a href="">ABOUT ME</a>
+        <a href="">WORK/CV</a>
+        <a href="">PROJECTS</a>
       </div>
       <div className="PicOfMe">
-          <img src={pork}></img>
+        {images.map((img: string, index: number) => (
+          <img 
+            key={index}
+            src={img} 
+            alt={`Slide ${index + 1}`}
+            className={index === currentImageIndex ? 'active' : ''}
+          />
+        ))}
       </div>
     </div>
   )
