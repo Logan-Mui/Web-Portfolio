bf0b738ca247e044361ec394f74077da1411a4d5
Logan Mui <l.mui@blindleister.de>
2025-10-23 18:50:50 +0200

    forgot to import styles

--- src/pages/community.tsx
+++ src/pages/community.tsx
@@ -1,9 +1,17 @@
 import Marquee_Collection from "../components/marquee";
+import "../styles/community/community.css";
 import "../styles/community/marquee.css";
 
 function community() {
   return (
     <div className="Page" id="community">
+      <div className="community-content">
+        <div className="community-widget">
+          <div className="community-links">
+
+          </div>
+        </div>
+      </div>
       <Marquee_Collection/>
     </div>
   )
