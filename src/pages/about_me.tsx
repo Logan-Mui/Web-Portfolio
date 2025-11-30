b5018ccdc68eab78199221ebd6ed252b83294726
Logan Mui <l.mui@blindleister.de>
2025-11-30 19:09:02 +0100

    changes to the boxing in work cv and also starting about me

--- src/pages/about_me.tsx
+++ src/pages/about_me.tsx
@@ -1,7 +1,27 @@
+import { InfoDropdown } from "../components/dropdown";
+
 function AboutMe() {
   return (
     <div className="Page" id="AboutMe">
-      <></>
+      <h1 className="text-6xl font-serif mb-12">About Me</h1>
+
+      <InfoDropdown title="Professional Background" content="
+      I'm a software engineer with a focus on full‑stack development, system
+      integrations, and building clean, scalable architectures. I enjoy
+      solving hard problems and crafting tools that make real workflows
+      easier."/>
+
+
+      <InfoDropdown title="Skills & Technologies" content="
+      oExperienced with: React, TypeScript, Spring Boot, Java, Docker,
+      distributed systems, API integrations, and more."/>
+
+
+      <InfoDropdown title="Outside of Work" content="
+      I enjoy photography, embedded electronics, PCB design, and learning
+      new technologies just for fun."/>
+
+
     </div>
   )
 }
